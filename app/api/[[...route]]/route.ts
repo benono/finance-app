b7df51e5e1eca218aca0b9a5ed584810b0ddb3cb
BenOno <bentoki.1213@gmail.com>
2024-05-18 07:14:11 -0700

    Fix follow Hono's Best Practices

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,9 +1,7 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel' // vercelの部分をawsとかdenoとか色々変えられるんだって
-import { z } from 'zod'
-import { zValidator } from '@hono/zod-validator';
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
-
+import authors from './authors'
+import books from './books'
 
 // later I have to remove this because plaid works with axios, doesn't work with edge
 // but now just keep it
@@ -11,41 +9,8 @@ export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
 
-app.get('/hello',
-    clerkMiddleware(),
-    (c) => {
-        const auth = getAuth(c)
-        if (!auth?.userId) {
-            return c.json({ error: "Unauthorized" })
-        }
-
-        return c.json({
-            message: 'Hello Next.js!',
-            userId: auth.userId
-        })
-    })
-    .get(
-        "/hello/:test",
-        zValidator("param", z.object({
-            test: z.number()
-        })),
-        (c) => {
-            const test = c.req.valid("param")
-            return c.json({
-                message: "Hello World",
-                test: test
-            })
-        })
-    .post("/",
-
-        zValidator("json", z.object({
-            name: z.string(),
-            userId: z.number(),
-        })),
-        (c) => {
-            const { name, userId } = c.req.valid("json")
-            return c.json({})
-        })
+app.route("/authors", authors)
+app.route("/books", books)
 
 export const GET = handle(app)
 export const POST = handle(app)
