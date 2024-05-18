c8b2a8577da9d31719064d888816a424aa83e979
BenOno <bentoki.1213@gmail.com>
2024-05-18 06:58:29 -0700

    Add validate middleware

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,6 +1,9 @@
 import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
-// ↑のvercelの部分をawsとかdenoとか色々変えられるんだって
+import { handle } from 'hono/vercel' // vercelの部分をawsとかdenoとか色々変えられるんだって
+import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator';
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
+
 
 // later I have to remove this because plaid works with axios, doesn't work with edge
 // but now just keep it
@@ -8,11 +11,41 @@ export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
 
-app.get('/hello', (c) => {
-    return c.json({
-        message: 'Hello Next.js!',
+app.get('/hello',
+    clerkMiddleware(),
+    (c) => {
+        const auth = getAuth(c)
+        if (!auth?.userId) {
+            return c.json({ error: "Unauthorized" })
+        }
+
+        return c.json({
+            message: 'Hello Next.js!',
+            userId: auth.userId
+        })
     })
-})
+    .get(
+        "/hello/:test",
+        zValidator("param", z.object({
+            test: z.number()
+        })),
+        (c) => {
+            const test = c.req.valid("param")
+            return c.json({
+                message: "Hello World",
+                test: test
+            })
+        })
+    .post("/",
+
+        zValidator("json", z.object({
+            name: z.string(),
+            userId: z.number(),
+        })),
+        (c) => {
+            const { name, userId } = c.req.valid("json")
+            return c.json({})
+        })
 
 export const GET = handle(app)
 export const POST = handle(app)
