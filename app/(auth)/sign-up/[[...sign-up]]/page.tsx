2b79dcdd9ec0fe969f90200205c2413e5bc36eed
BenOno <bentoki.1213@gmail.com>
2024-05-17 21:59:32 -0700

    Add auth router group

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+const SignUpPage = () => {
+    return (
+        <div>
+            Sign up page
+        </div>
+    )
+}
+// you can even name it as X, all the matter is doing default export
+export default SignUpPage
