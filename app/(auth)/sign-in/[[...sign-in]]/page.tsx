cc3428d979e22ba882821985caf384e57b05c72a
BenOno <bentoki.1213@gmail.com>
2024-05-17 22:42:48 -0700

    Update sign-in layout

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,21 @@ import { SignIn } from "@clerk/nextjs";
 
 const SignInPage = () => {
     return (
-        <SignIn path="/sign-in" />
+        <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+            <div className="h-full lg:glex flex-col items-center justify-center px-4">
+                <div className="text-center space-y-4 pt-16">
+                    <h1 className="font-bold text-3xl text-[#2E2A47]">
+                        Welcome Back!
+                    </h1>
+                    <p className="text-base text-[#7E8CA0]">
+                        Login or Create account to get back to your dashboard
+                    </p>
+                    <div className="flex items-center justify-center mt-8">
+                        <SignIn path="/sign-in" />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
