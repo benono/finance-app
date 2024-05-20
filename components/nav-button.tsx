180d5208ee698a07418c07caea3d89f0fe0502ff
BenOno <bentoki.1213@gmail.com>
2024-05-20 09:30:50 -0700

    Fix typo

--- components/nav-button.tsx
+++ components/nav-button.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 import { Button } from "./ui/button"
 import { cn } from "@/lib/utils"
 
+
 type Props = {
     href: string
     label: string
@@ -20,7 +21,7 @@ export const NavButton = ({
                 size="sm"
                 variant="outline"
                 className={cn(
-                    "w-full lg:w-auto justify-between font-normal hover:bg-white/20 hover:text-white border-none focus-visible:ring-0-offset-0 focus-visible:rint-transparent outline-none text-white focus:bg-white/30 transition",
+                    "w-full lg:w-auto justify-between font-normal hover:bg-white/20 hover:text-white border-none focus-visible:ring-0-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transition",
                     isActive ? "bg-white/10 text-white" : "bg-transparent",
                 )}
             >
