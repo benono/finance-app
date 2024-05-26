ec514f0a3a3d9488caa14f67c42e4aa28a82f0aa
BenOno <bentoki.1213@gmail.com>
2024-05-26 16:53:00 -0700

    Use Apptype https://hono.dev/guides/rpc#using-rpc-with-larger-applications

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,16 +1,17 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel' // vercelの部分をawsとかdenoとか色々変えられるんだって
-import authors from './authors'
-import books from './books'
 
+import accounts from "./accounts";
 // later I have to remove this because plaid works with axios, doesn't work with edge
 // but now just keep it
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
 
-app.route("/authors", authors)
-app.route("/books", books)
-
+const routes = app
+    .route("/accounts", accounts)
 export const GET = handle(app)
 export const POST = handle(app)
+
+export type AppType = typeof routes
+// refer 2:14:55 at youtube
