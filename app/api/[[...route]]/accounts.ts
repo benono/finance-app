cabe37e76dce9c18613d69f3053567c3b8d6cb70
BenOno <bentoki.1213@gmail.com>
2024-05-28 12:26:10 -0700

    Add Accounts Post API

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,9 +1,12 @@
 import { Hono } from 'hono'
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import { eq } from "drizzle-orm"
+//import {createId} from "@paralleldrive/cuid2"
+import { zValidator } from "@hono/zod-validator"
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+
 
 import { db } from '@/db/drizzle'
-import { accounts } from '@/db/schema'
+import { accounts, insertAccountSchema } from '@/db/schema'
 
 const app = new Hono()
     .get(
@@ -22,6 +25,27 @@ const app = new Hono()
                 .from(accounts)
                 .where(eq(accounts.userId, auth.userId))
             return c.json({ data })
+        })
+
+    .post(
+        "/",
+        clerkMiddleware(),
+        zValidator("json", insertAccountSchema.pick({
+            name: true,
+        })),
+        async (c) => {
+            const auth = getAuth(c)
+            const values = c.req.valid("json")
+            if (!auth?.userId) {
+                return c.json({ error: "Unauthorized" }, 401)
+            }
+
+            const data = await db.insert(accounts).values({
+                id: "test",
+                userId: auth.userId,
+                ...values,
+            })
+            return c.json({})
         })
 
 export default app
