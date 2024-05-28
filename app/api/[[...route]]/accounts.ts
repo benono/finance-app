11a7bffcbb471e33813e842ad468e56ac34b3f26
BenOno <bentoki.1213@gmail.com>
2024-05-28 12:15:51 -0700

    Update fix error handling

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,7 +1,6 @@
 import { Hono } from 'hono'
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import { eq } from "drizzle-orm"
-import { HTTPException } from "hono/http-exception"
 
 import { db } from '@/db/drizzle'
 import { accounts } from '@/db/schema'
@@ -13,9 +12,7 @@ const app = new Hono()
         async (c) => {
             const auth = getAuth(c)
             if (!auth?.userId) {
-                throw new HTTPException(401, {
-                    res: c.json({ error: "unauthorized" }, 401)
-                })
+                return c.json({ error: "unauthorized" }, 401)
             }
             const data = await db
                 .select({
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,18 +2,13 @@ import { Hono } from 'hono'
 import { handle } from 'hono/vercel' // vercelの部分をawsとかdenoとか色々変えられるんだって
 
 import accounts from "./accounts";
-import { HTTPException } from 'hono/http-exception';
 // later I have to remove this because plaid works with axios, doesn't work with edge
 // but now just keep it
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
-app.onError((err, c) => {
-    if (err instanceof HTTPException) {
-        return err.getResponse()
-    }
-    return c.json({ error: "Internal error" }, 500)
-})
+
+
 const routes = app
     .route("/accounts", accounts)
 export const GET = handle(app)
