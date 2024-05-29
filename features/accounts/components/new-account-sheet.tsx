be0f69a9cfcc5df5646e82d003273eff2d4c27b3
BenOno <bentoki.1213@gmail.com>
2024-05-29 12:34:51 -0700

    Add account-form

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,10 +1,30 @@
+import { z } from "zod"
 import { useNewAccount } from "@/features/hooks/use-new-account"
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet"
-import { use } from "react"
+import { AccountForm } from "@/features/accounts/components/account-form"
+import { insertAccountSchema } from "@/db/schema"
+
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+} from "@/components/ui/sheet"
+
+const formSchema = insertAccountSchema.pick({
+    name: true
+})
+
+type FormValues = z.input<typeof formSchema>
 
 
 export const NewAccountSheet = () => {
     const { isOpen, onClose } = useNewAccount()
+
+    const onSubmit = (values: FormValues) => {
+        console.log({ values })
+        console.log('EIEI')
+    }
     return (
         <Sheet open={isOpen} onOpenChange={onClose}>
             <SheetContent className="space-y-4">
@@ -16,6 +36,14 @@ export const NewAccountSheet = () => {
                         Create a new account to track your transactions
                     </SheetDescription>
                 </SheetHeader>
+                <AccountForm
+                    onSubmit={onSubmit}
+                    onDelete={() => { }}
+                    disabled={false}
+                    defaultValues={{
+                        name: ""
+                    }}
+                />
             </SheetContent>
         </Sheet>
     )
