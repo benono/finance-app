47db63a6e11af7b33949deddb4ecb075bf63e44a
BenOno <bentoki.1213@gmail.com>
2024-05-29 03:09:23 -0700

    Update AccountSheet state handling

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,8 +1,12 @@
+import { useNewAccount } from "@/features/hooks/use-new-account"
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet"
+import { use } from "react"
+
 
 export const NewAccountSheet = () => {
+    const { isOpen, onClose } = useNewAccount()
     return (
-        <Sheet open>
+        <Sheet open={isOpen} onOpenChange={onClose}>
             <SheetContent className="space-y-4">
                 <SheetHeader>
                     <SheetTitle>
