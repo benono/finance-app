3dc8e808887b7aaccc7fda24e7a5a8d0a16bc108
BenOno <bentoki.1213@gmail.com>
2024-06-03 12:45:29 -0700

    Add filterling

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -3,29 +3,14 @@
 import { Plus } from "lucide-react"
 
 import { useNewAccount } from "@/features/hooks/use-new-account"
-import {
-    Card,
-    CardContent,
-    CardHeader,
-    CardTitle
-} from "@/components/ui/card"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { DataTable } from "@/components/data-table"
-import { Payment,columns } from "./columns"
+import { Payment, columns } from "./columns"
 
 async function getData(): Promise<Payment[]> {
-    // Fetch data from your API here.
-    return [
-      {
-        id: "728ed52f",
-        amount: 100,
-        status: "pending",
-        email: "m@example.com",
-      },
-      // ...
-    ]
-  }
-const data: Payment[] = [
+  // Fetch data from your API here.
+  return [
     {
       id: "728ed52f",
       amount: 100,
@@ -34,26 +19,34 @@ const data: Payment[] = [
     },
     // ...
   ]
+}
+const data: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  // ...
+]
 const AccountsPage = () => {
-    const newAccount = useNewAccount()
-    return (
-        <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
-            <Card className="border-none drop-shadow-sm">
-                <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
-                    <CardTitle className="text-xl line-clamp-1">
-                    Accounts Page
-                    </CardTitle>
-                    <Button onClick={newAccount.onOpen} size="sm">
-                        <Plus className="size-4 mr-2" />
-                        Add new
-                    </Button>
-                </CardHeader>
-                <CardContent>
-                    <DataTable columns={columns} data={data} />
-                </CardContent>
-            </Card>
-        </div>
-    )
-} 
+  const newAccount = useNewAccount()
+  return (
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="text-xl line-clamp-1">Accounts Page</CardTitle>
+          <Button onClick={newAccount.onOpen} size="sm">
+            <Plus className="size-4 mr-2" />
+            Add new
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable filterKey="email" columns={columns} data={data} />
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
 
 export default AccountsPage
--- components/data-table.tsx
+++ components/data-table.tsx
@@ -3,8 +3,10 @@
 import * as React from "react"
 import {
   ColumnDef,
+  ColumnFiltersState,
   SortingState,
   flexRender,
+  getFilteredRowModel,
   getPaginationRowModel,
   getCoreRowModel,
   getSortedRowModel,
@@ -20,17 +22,23 @@ import {
   TableRow,
 } from "@/components/ui/table"
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
   data: TData[]
+  filterKey: string
 }
 
 export function DataTable<TData, TValue>({
   columns,
   data,
+  filterKey,
 }: DataTableProps<TData, TValue>) {
   const [sorting, setSorting] = React.useState<SortingState>([])
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  )
   const table = useReactTable({
     data,
     columns,
@@ -38,13 +46,26 @@ export function DataTable<TData, TValue>({
     getPaginationRowModel: getPaginationRowModel(),
     onSortingChange: setSorting,
     getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
     state: {
       sorting,
+      columnFilters,
     },
   })
 
   return (
     <div>
+      <div className="flex items-center py-4">
+        <Input
+          placeholder={`Filter ${filterKey}...`}
+          value={(table.getColumn(filterKey)?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("email")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+      </div>
       <div className="rounded-md border">
         <Table>
           <TableHeader>
